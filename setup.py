6a5d8d9a3ee01aaec5429b2d5f5f9196b8cbb2fd
Moise Rousseau <moise.rousseau@usherbrooke.ca>
2023-10-12 15:30:58 -0400

    Correct version number not found when installing from TestPyPi

--- setup.py
+++ setup.py
@@ -4,20 +4,26 @@ from setuptools import find_packages
 import skbuild
 import pathlib
 
-#ne pas importer numpy, et pas de try/except sur skbuild, mais plutot mentionner son installation dans le readme
+try:
+    #when install from the git repository
+    import os
+    import subprocess
+    #get the git tag and commit number
+    git_hash = str(subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']))
+    version = str(subprocess.check_output(['git', 'describe', '--tags']))
+    git_hash = git_hash[2:-3]
+    version = version[2:-3]
+    version = version.rsplit("-")[0]
+    #write it in pyqcm folder
+    fout = open("pyqcm/qcm_git_hash.py", "w")
+    fout.write("git_hash = '{:s}'\n".format(git_hash))
+    fout.write("version = '{:s}'\n".format(version))
+    fout.close() 
+except:
+    #install outside git repository, the subprocess calling git should fail
+    fin = open("pyqcm/qcm_git_hash.py", "r")
+    version = fin.readlines()[1].split(' ')[1].split('\'')[1]
 
-import os
-import subprocess
-git_hash = str(subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']))
-version = str(subprocess.check_output(['git', 'describe', '--tags']))
-git_hash = git_hash[2:-3]
-version = version[2:-3]
-version = version.rsplit("-")[0]
-
-fout = open("pyqcm/qcm_git_hash.py", "w")
-fout.write("git_hash = '{:s}'\n".format(git_hash))
-fout.write("version = '{:s}'\n".format(version))
-fout.close() 
  
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
